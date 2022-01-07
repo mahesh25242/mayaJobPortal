7e427bd207149850730fe4079ebe8322723cd4eb
Mahesh K S <mahesh25242@gmail.com>
2022-01-07 11:48:10 +0530

    xc

--- ui/src/pages/admin/categories/CategoriesList.tsx
+++ ui/src/pages/admin/categories/CategoriesList.tsx
@@ -7,31 +7,33 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CreateCategory from './CreateCategory';
+import { useDispatch, useSelector } from 'react-redux';
+import { categoryFetch, fetchCategories } from '../../../api/catgories/CategorySlice';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
+import Link from '@mui/material/Link';
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 export default function CategoriesList() {
+
+    const [open, setOpen] = React.useState(false);
+
+    const dispatch = useDispatch();
+    const { categories } = useSelector(categoryFetch);
+    
+    
+    React.useEffect(() => {
+      dispatch(fetchCategories());    
+  }, []);
+  
   return (
     <TableContainer component={Paper}>
+        <CreateCategory open={open} setOpen={setOpen}/>
        <Typography gutterBottom variant="h5" component="div">
           Categories
         </Typography>
+      <Button variant="contained" onClick={(e)=> setOpen(true)}>Careate New</Button>
       <Table  aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -42,17 +44,21 @@ export default function CategoriesList() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {categories.categories && categories.categories.map((row:any) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>              
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">
+                <Link href='#' onClick={() => setOpen(true)}>{row.name}</Link>
+              </TableCell>
+              <TableCell align="right">{row.status_text}</TableCell>              
+              <TableCell align="right">                
+                <Button>Delete</Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
