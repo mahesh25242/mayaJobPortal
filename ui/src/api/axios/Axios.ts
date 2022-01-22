cf24417a9362ca181cdb7c636fcbb6cdfc442139
Mahesh K S <mahesh25242@gmail.com>
2022-01-22 12:28:51 +0530

    updated still here in fiya.in

--- ui/src/api/axios/Axios.ts
+++ ui/src/api/axios/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: 'http://localhost:8000/v1/',
+	baseURL: 'https://fiya.in/api/public/v1/' //'http://localhost:8000/v1/',
 });
 // instance.defaults.withCredentials = true;
 const token = localStorage.getItem('token');
