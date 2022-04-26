6425fb4a1baa7d0804c28714ef0b23c775759037
Mahesh K S <mahesh25242@gmail.com>
2022-04-26 07:55:18 +0530

    fix

--- ui/src/pages/admin/Admin.tsx
+++ ui/src/pages/admin/Admin.tsx
@@ -4,7 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import {
   checkLogin,
-  getAuth
+  getAuth,
+  signOut
 } from "../../api/users/AuthenticationSlice";
 import { AlternateEmailTwoTone } from "@mui/icons-material";
 import Home from "./home/Home";
@@ -13,19 +14,21 @@ import SettingLists from "./settings/SettingLists";
 import EmployersList from "./employers/EmployersList";
 import JobSekkersList from "./jobSekkers/JobSekkersList";
 import BlogLists from "./blogs/BlogLists";
+import instance from "../../api/axios/Axios";
+import { useLoggedUserQuery } from "../../api/rtk/user";
 
 
 export default function Admin(){ 
   const dispatch = useDispatch();
   const { token } = useSelector(getAuth);
-
+  const { data, error, isLoading } = useLoggedUserQuery('');  
   
 
     return (<>    
         <Routes>   
         <Route path="login" element={<Login />} />        
           {
-            token &&   <Route path="/*" element={
+            data && token &&   <Route path="/*" element={
               <PrivateRoute>
                 <DashboardLayout />
               </PrivateRoute>}>
@@ -38,10 +41,9 @@ export default function Admin(){
       </>);
 }
 
-function PrivateRoute({ children }: any) {    
-  
-    const { token } = useSelector(getAuth);       
-    return !!token.token ? children :   <Navigate to="/admin/login" />;
+function PrivateRoute({ children }: any) {  
+  const { token } = useSelector(getAuth);        
+  return !!token.token ? children :   <Navigate to="/admin/login" />; 
 }
   
 
